7c081505e5ccdfc6178855d65499121ef1e58e53
Martin Ling <martin-git@earth.li>
2015-03-24 23:15:32 +0000

    Generate a config.h rather than passing defines on command line.
    Fixes bug #553.

--- libserialport_internal.h
+++ libserialport_internal.h
@@ -18,6 +18,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
+
 #ifdef __linux__
 #define _BSD_SOURCE // for timeradd, timersub, timercmp
 #endif
