c37c9f348d015f1fcec1d8597458418bf1300565
Daniel Elstner <daniel.kitta@gmail.com>
2015-08-27 11:46:06 +0200

    autogen.sh: Create autostuff/ dir if it does not exist

--- autogen.sh
+++ autogen.sh
@@ -22,4 +22,5 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
+test -d "$srcdir/autostuff" || mkdir "$srcdir/autostuff"
 autoreconf --force --install --verbose "$srcdir"
