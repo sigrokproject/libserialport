f6abee5c78b1d4d7bcfd8bd08bd57baa5f011ba2
Martin Ling <martin-git@earth.li>
2020-01-24 08:02:36 +0000

    Relax timing test because it's too tight for Windows.
    On my test system, a delay of 1016ms was measured after a
    sleep of 1000ms.

--- test_timing.c
+++ test_timing.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	time_sub(&b, &a, &c);
 	printf("Measured: %ums\n", time_as_ms(&c));
 	assert(time_as_ms(&c) >= 999);
-	assert(time_as_ms(&c) <= 1001);
+	assert(time_as_ms(&c) <= 1050);
 	printf("Starting 3s timeout\n");
 	timeout_start(&to, 3000);
 	printf("Time to wait: %dms\n", timeout_remaining_ms(&to));
